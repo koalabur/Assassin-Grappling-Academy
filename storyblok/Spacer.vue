e51724a55f2218df614b784cc98fb2018b0525fb
Cameron Roberts <44409341+koalabur@users.noreply.github.com>
2024-01-02 13:53:43 -0800

    feat: add margins (#14)

--- storyblok/Spacer.vue
+++ storyblok/Spacer.vue
@@ -11,14 +11,18 @@ const { blok } = data.value;
 
 const desktopHeight = computed(() => `${blok.desktop}px`);
 const mobileHeight = computed(() => `${blok.mobile}px`);
+const marginTop = computed(() => `${blok.marginTop || 0}px`);
+const marginTopMobile = computed(() => `${blok.marginTopMobile || 0}px`);
 </script>
 
 <style lang="scss" scoped>
 .spacer {
   height: v-bind(mobileHeight);
+  margin-top: v-bind(marginTopMobile);
 
   @include mq(lg) {
     height: v-bind(desktopHeight);
+    margin-top: v-bind(marginTop);
   }
 }
 </style>
