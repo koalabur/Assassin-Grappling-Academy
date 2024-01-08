2152ed395aa544db651129aa76b8423e633e560f
Cameron Roberts <44409341+koalabur@users.noreply.github.com>
2024-01-07 22:00:41 -0800

    bug: fix previews (#18)

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -26,7 +26,7 @@ if (!story.value) {
 }
 
 onMounted(() => {
-  if (isPreview && story.value && story.value.id) {
+  if (story.value && story.value.id) {
     useStoryblokBridge(story.value.id, (evStory) => (story.value = evStory));
   }
 });
